010b047f20e2e4db18cf0b237063f57b69a19d94
Javier Gonzalez <javig56@gmail.com>
2018-04-04 15:37:24 -0700

    upload Simple CRUD c# assignment

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DbConnection;
+
+namespace SimpleCRUD
+{
+    class Program
+    {
+        public static void Show(){
+            var users = DbConnector.Query("SELECT * FROM users");
+            foreach (var user in users){
+            Console.WriteLine("User Info:");
+                foreach(KeyValuePair<string, object> entry in user){
+                    Console.WriteLine(entry);
+                }
+            }
+        }
+        public static void Create(){
+            Console.WriteLine("Create a New User");
+            Console.WriteLine("Add First Name:");
+            string Fname = Console.ReadLine();
+            Console.WriteLine("Now add a Last Name");
+            string Lname = Console.ReadLine();
+            Console.WriteLine("Lastly Add the Favorite Number");
+            string FavNum = Console.ReadLine();
+            var query = $"INSERT INTO users (FirstName, LastName, FavoriteNumber) VALUES ('{Fname}', '{Lname}', {FavNum})";
+            DbConnector.Execute(query);
+            Show();
+        }
+        static void Main(string[] args)
+        {
+            Show();
+            Create();
+        }
+    }
+}
